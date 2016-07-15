8835443458e94e30913d409aa982c99e7f68cd8b
Naga Kiran <nagakiran@juniper.net>
2016-07-15 19:04:53 +0530

    Updated UI with vRouter UVE changes done for reporting physical bandwidth usage
    Change-Id: I7a21947d2fbfb599958ea5d4a27e880be3d19ef5

--- webroot/monitor/infrastructure/vrouter/ui/js/views/VRouterDetailsBandwidthLineChartView.js
+++ webroot/monitor/infrastructure/vrouter/ui/js/views/VRouterDetailsBandwidthLineChartView.js
@@ -32,8 +32,8 @@ define([
                                 viewConfig: {
                                     parseFn: function (response) {
                                         var dimensions = ['MAX(flow_rate.active_flows)',
-                                                          'phy_if_band.in_bandwidth_usage',
-                                                          'phy_if_band.out_bandwidth_usage'];
+                                                          'phy_band_in_bps',
+                                                          'phy_band_out_bps'];
                                         var axisLabels = ['Flow Rate','Bandwidth In','Bandwidth Out'];
                                         var options = {dimensions:dimensions,axisLabels:axisLabels};
                                         return ctwp.parseLineChartDataForVRouterBandwidth(response,options);
